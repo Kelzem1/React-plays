4d922dc8c30fd8024a2f1f33178c0da1a0d5e6da
Kelzem1 <kelzemrod@gmail.com>
2023-04-24 12:07:56 +0200

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+
+const MyButton = () =>{
+  return (
+    
+      <button>
+        Im a button
+      </button>
+    )
+  }
+
+const App = () =>{
+  return(
+    <div>
+      <h1>Hello Welcome to my app</h1>
+      <MyButton />
+    </div>
+  )
+}
+
+
+export default App
